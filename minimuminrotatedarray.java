85d6b2cd7eb9f16f77dff6113eee4b7da84409ba
42-sav <84896130+42-sav@users.noreply.github.com>
2023-01-02 22:51:58 +0530

    Add files via upload

--- minimuminrotatedarray.java
+++ minimuminrotatedarray.java
@@ -1,31 +1,23 @@
 class Solution {
     public int findMin(int[] nums) {
-        int n = nums.length;
-        int mid = n/2;
+        int n = nums.length-1;
+         
         int result = 0;
         int i=0;
+       
         //find the middle element and find if its larger than the next integer and if larger then return the right side of the array and the first int in teh right is the minimum
-        if(mid==0){
-             result=nums[0];
-           
-        }
-        else if(mid>1 && nums[mid]>nums[mid+1]){//index out of bounds for 2 elememt array,try to solve without using mid+1 (solved)
-           
-             result = nums[mid+1];
 
-        }
-        else{
-            while(i<mid){
-                if(nums[i]<nums[i+1]){
-                    result = nums[i];
-                    break;
-                }
-                else{
-                    result=nums[0];
-                    break; //61 test cases passed 
-                }
+//iterate using while loop and use recusrion binary search 
+        while(i<n){
+            int mid = i + (n-i)/2;
+            if(nums[mid]>nums[n]){
+                i = mid+1;
             }
+            if(nums[mid]<nums[n]){
+                n = mid;
+            }
+            
         }
-        return result;
+        return nums[i];
     }
 }
