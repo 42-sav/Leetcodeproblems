1fb1ba7be418c69b8706b85ef5737dba3e3c3a88
42-sav <84896130+42-sav@users.noreply.github.com>
2023-01-12 21:17:26 +0530

    Add files via upload

--- containerwithmostwater.java
+++ containerwithmostwater.java
@@ -3,22 +3,35 @@ class Solution {
         //find the start and end and area 
         int start = 0;
         int end=height.length-1;
-        int mid = height.length/2;
+        //int mid = height.length/2;
         //int[] area ;
-        List<Integer> area = new ArrayList<Integer>();
+        //List<Integer> area = new ArrayList<Integer>();
         int res = 0;
 
-        for( start=0;start<=mid;start++){
-            
-            area.add(height[start]*height[end]);
-            end= end--;
-        }
-        for(int i =0;i<=area.size()-1;i++){
-            if(i>res){
-                res = i;
+        //boolean flag = true;
+        //think of two pointer methods and conditions,select one pointer from start and one for end and then move the smaller pointer as we want max area and if we have equal move both as we have calculated both then its of no use coz we can fill only till minimum of the  heights.
+
+        while(start<end){
+            int width = end - start;
+            int h = Math.min(height[start],height[end]);
+            int area = width*h;
+             res = Math.max(res,area);
+
+            if(height[start] < height[end]){
+                start++;
             }
+            else if (height[start]>height[end]){
+                end--;
+            }
+            else{
+                start++;
+                end--;
+            }
+            
+            
+           // end= end--;
         }
-        //1 test case passed , need to think of other ways 
+        
 
 
      return res;   
