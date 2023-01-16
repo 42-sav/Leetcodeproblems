6dd04e7dbf28d01677bede5f7f7f6027be9d0295
42-sav <84896130+42-sav@users.noreply.github.com>
2023-01-16 19:30:17 +0530

    Add files via upload

--- stairsproblem.java
+++ stairsproblem.java
@@ -2,19 +2,22 @@ class Solution {
     public int climbStairs(int n) {
         //combination problem ,find factorial and apply the formula-wrong approach
         //use the test cases and find patterns 
-        int fact = 1;
-        int res = 0;
-        for(int i = 1,i<=n;i++){
-            fact = fact * i;
-        }
-        if(n = 1){
-            res = 1;
-        }
-        else{
-            res = fact
-        }
+        int a =1;
+        int b=2;
+        int out = 0;
 
+        if(n<=2){
+            return n;
 
-        
+        }
+        for(int i=3;i<=n;i++){
+            int res = a+b;
+            a=b;
+            b=res;
+            out=res;
+            
+        }
+        return out;
     }
+    
 }
